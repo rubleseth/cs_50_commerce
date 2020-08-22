665d8970935786aeb9028a2fdb7d8a5acba81953
Seth <rubleseth@gmail.com>
2020-08-22 14:26:29 +0200

    Update to Commerce

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 
 class User(AbstractUser):
+    pass
+
+
+class Listing(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    image = models.ImageField
+
+
+class Bid(models.Model):
+    pass
+
+
+class Comments(models.Model):
+    pass
+
+
+class Watchlist(models.Model):
     pass
